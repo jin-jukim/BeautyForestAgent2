e50483328d4e883c174e4560a388232fa53c0812
Jinju Kim <swinging13@naver.com>
2019-06-24 14:59:00 +0900

    #2 BeautyForestAgent02 코드 구현 by 3103 김진주

--- BeautyForestAgent/Form1.cs
+++ BeautyForestAgent/Form1.cs
@@ -47,6 +47,56 @@ namespace BeautyForestAgent
         {
             VisibleChange(true);
         }
+
+        private void 끝내기ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "종료합니다.";
+            Application.ExitThread();  // 어플리케이션 쓰레드 완전 종료
+        }
+
+        private void 수정ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowFormModify();
+        }
+
+        private void ShowFormModify()
+        {
+            this.statusMessage.Text = "프로필 수정";
+            FormModify m = new FormModify();
+            m.Title = "프로필 수정";
+            m.ShowDialog();
+        }
+
+        private void 추가ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowFromAdd();
+        }
+
+        private void ShowFromAdd()
+        {
+            this.statusMessage.Text = "프로필 추가";
+            FormAdd m = new FormAdd();
+            m.Title = "프로필 추가";
+            m.ShowDialog();
+        }
+
+        private void 이프로그램은ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "이 프로그램은.. 창을 띄웁니다.";
+            FormAbout m = new FormAbout();
+            m.Title = "이 프로그램은.. 창 띄우기";
+            m.Show();
+        }
+
+        private void BtnModify_Click(object sender, EventArgs e)
+        {
+            ShowFormModify();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            ShowFromAdd();
+        }
     }
 
 }
