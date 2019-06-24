a4d24b8014df6eda36b75112739087cc776fe05b
Jinju Kim <swinging13@naver.com>
2019-06-24 14:44:46 +0900

    #1 BeautyForestAgent2 폼 디자인 by 3103 김진주

--- BeautyForestAgent/Form1.Designer.cs
+++ BeautyForestAgent/Form1.Designer.cs
@@ -240,7 +240,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "주Beauty Forest Agent v0.2 by 3103 김진";
+            this.Text = "Beauty Forest Agent v0.2 by 3103 김진주";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.cmsMenu.ResumeLayout(false);
